49957e0070fd56fbab98f2a76627541c4df4268b
abrar <abrar@hnxlabs.com>
2018-10-18 20:28:20 -0700

    have the script in sudo

--- upgrade-plex.sh
+++ upgrade-plex.sh
@@ -2,7 +2,7 @@
 echo "plex: downloading"
 curl -sL -o ./plex.deb "https://plex.tv/downloads/latest/1?channel=16&build=linux-ubuntu-x86_64&distro=ubuntu&X-Plex-Token=4Jnu8SqixsEwiJFWK9D4"
 echo "plex: installing"
-dpkg -i ./plex.deb
+sudo dpkg -i ./plex.deb
 echo "plex: install result $?"
 echo "plex: cleanup"
 rm ./plex.deb
