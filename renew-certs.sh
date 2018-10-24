91a7f7684ae30972d56f313d30ac3713511a7f69
abrar-hnxlabs <au8832@gmail.com>
2018-10-24 06:38:12 +0000

    add new script and fix renew certs

--- renew-certs.sh
+++ renew-certs.sh
@@ -2,6 +2,6 @@
 
 sudo service haproxy stop
 sudo letsencrypt renew --standalone
-sudo cat /etc/letsencrypt/live/plex.hnxlabs.com/fullchain.pem /etc/letsencrypt/live/plex.hnxlabs.com/privkey.pem > /etc/ssl/private/plex.hnxlabs.com.pem 
-sudo cat /etc/letsencrypt/live/bt.hnxlabs.com/fullchain.pem /etc/letsencrypt/live/bt.hnxlabs.com/privkey.pem > /etc/ssl/private/bt.hnxlabs.com.pem
+sudo cat /etc/letsencrypt/live/plex.hnxlabs.com/fullchain.pem /etc/letsencrypt/live/plex.hnxlabs.com/privkey.pem | sudo tee /etc/ssl/private/plex.hnxlabs.com.pem 
+sudo cat /etc/letsencrypt/live/bt.hnxlabs.com/fullchain.pem /etc/letsencrypt/live/bt.hnxlabs.com/privkey.pem | sudo tee /etc/ssl/private/bt.hnxlabs.com.pem
 sudo service haproxy start
---
+++ system-init.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+sudo apt-get update
+sudo add-apt-repository ppa:transmissionbt/ppa
+sudo add-apt-repository ppa:certbot/certbot
+sudo apt-get install -y certbot\
+       		transmission-cli \
+	       	transmission-common \
+	       	transmission-daemon \
+		haproxy \
+		ddclient \
+		software-properties-common \
+		samba
