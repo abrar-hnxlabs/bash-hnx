56ed679a898eb97051e3a74e7e392370d3a9ef76
abrar <abrar@hnxlabs.com>
2018-12-20 20:52:15 -0800

    add dynamic checking and creation of ddclient config file

--- init.sh
+++ init.sh
@@ -1,4 +1,26 @@
-#! /bin/sh
+#! /bin/bash
+
+if [ "$(whoami)" != "root" ]; then
+	echo "Must run script as root to execute"
+	exit 1
+fi
+
+echo "starting ddclient config, get the credentials from google domains"
+echo "provide username for bt.hnxlabs.com"
+read btusername
+echo "provide password for bt.hnxlabs.com"
+read btpassword
+echo "use=web, web=ipconfig.io\nprotocol=googledomains,\nlogin=$btusername,\npassword=$btpassword\nbt.hnxlabs.com" > /etc/ddclient.conf
+
+echo "provide username for plex.hnxlabs.com"
+read plexusername
+echo "provide password for plex.hnxlabs.com"
+read plexpassword
+echo "\nuse=web, web=ipconfig.io\nprotocol=googledomains,\nlogin=$plexusername,\npassword=$plexpassword\nbt.hnxlabs.com" >> /etc/ddclient.conf
+
+systemctl restart ddclient
+
+letsencrypt certonly --standalone -d bt.hnxlabs.com,plex.hnxlabs.com 
 
 cp -v smb.simple.conf /etc/samba/smb.conf
 cp -v haproxy.cfg /etc/haproxy/haproxy.cfg
@@ -7,6 +29,16 @@ systemctl reload transmission-daemon.service
 systemctl restart haproxy
 systemctl restart smbd
 
-echo "Setting up the groups"
-usermod -a -G plex debian-transmission
-usermod -a -G plex abrar
+echo "adding users to plex group"
+
+if [[ "$(groups abrar)" =~ .*plex.* ]]; then
+	echo "User Already has plex group"
+else
+	usermod -a -G plex abrar
+fi
+
+if [[ "$(groups debian-transmission)" =~ .*plex.* ]]; then
+	echo "User Already has plex group"
+else
+	usermod -a -G plex debian-transmission
+fi
