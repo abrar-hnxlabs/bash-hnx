b86e1da5a5cda1f3bcfbc236e9cab40c0d3be77c
abrar <abrar@hnxlabs.com>
2018-12-21 22:05:22 -0800

    add confirm dialog for init start

--- system-init.sh
+++ system-init.sh
@@ -1,15 +1,19 @@
 #! /bin/bash
 
-sudo apt-get update
-sudo add-apt-repository ppa:transmissionbt/ppa
-sudo add-apt-repository ppa:certbot/certbot
-sudo apt-get install -y certbot\
-       		transmission-cli \
-	       	transmission-common \
-	       	transmission-daemon \
-		haproxy \
-		software-properties-common \
-		samba
+echo "Confirm you want to start system init? Y/N"
+read answer
+if [ "$answer" == "Y" ] || [ "$answer" == "y" ]; then
+  sudo apt-get update
+  sudo add-apt-repository ppa:transmissionbt/ppa
+  sudo add-apt-repository ppa:certbot/certbot
+  sudo apt-get install -y certbot\
+        		transmission-cli \
+         	       	transmission-common \
+	         	transmission-daemon \
+	        	haproxy \
+		        software-properties-common \
+		        samba
 
-sudo ./upgrade-plex.sh
-sudo ./init.sh
+  sudo ./upgrade-plex.sh
+  sudo ./init.sh
+fi
