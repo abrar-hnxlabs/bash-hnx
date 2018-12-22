369afde1386745595deb0d6695587464f177ffbe
abrar <abrar@hnxlabs.com>
2018-12-21 16:26:11 -0800

    add root directory path

--- dyn-dns.sh
+++ dyn-dns.sh
@@ -1,19 +1,21 @@
 #! /bin/bash
 
-if [ ! -r auth1.dec ]; then
+rootDir="/home/abrar/ubuntu-configs"
+if [ ! -r "$rootDir/auth1.dec" ]; then
 	echo "Decrypting first secret, password hint: qQ7"
-	openssl enc -d -aes256 -base64 -in ./auth1.enc -out auth1.dec
+	openssl enc -d -aes256 -base64 -in "$rootDir/auth1.enc" -out "$rootDir/auth1.dec"
 fi
 
-if [ ! -r auth2.dec ]; then
+if [ ! -r "$rootDir/auth2.dec" ]; then
 	echo "Decrypting second secret, password hint: qQ7"
-	openssl enc -d -aes256 -base64 -in ./auth2.enc -out auth2.dec
+	openssl enc -d -aes256 -base64 -in "$rootDir/auth2.enc" -out "$rootDir/auth2.dec"
 fi
 
-auth1=$(cat auth1.dec)
-auth2=$(cat auth2.dec)
+auth1=$(cat "$rootDir/auth1.dec")
+auth2=$(cat "$rootDir/auth2.dec")
 date
-echo "\n"
+echo " "
 curl -XPOST -H "Authorization: Basic $auth1" 'https://domains.google.com/nic/update?hostname=bt.hnxlabs.com'
-echo "\n"
+echo " "
 curl -XPOST -H "Authorization: Basic $auth2" 'https://domains.google.com/nic/update?hostname=plex.hnxlabs.com'
+echo " "
