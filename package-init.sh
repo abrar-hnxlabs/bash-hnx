495c211028970d7db850f918b855cff9c9780825
abrar-hnxlabs <abrar@hnxlabs.com>
2019-01-08 20:00:04 -0800

    add a cron mailer script

--- package-init.sh
+++ package-init.sh
@@ -14,7 +14,8 @@ if [ "$answer" == "Y" ] || [ "$answer" == "y" ]; then
 		        software-properties-common \
 		        samba \
 			whois
-
+  curl -sL https://deb.nodesource.com/setup_11.x | sudo -E bash -
+  sudo apt-get install -y nodejs
   sudo ./upgrade-plex.sh
   sudo ./init.sh
 fi
