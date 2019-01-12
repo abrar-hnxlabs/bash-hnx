bad9092b9015995b81b84d66c2de60c6cb53037d
abrar-hnxlabs <abrar@hnxlabs.com>
2019-01-12 08:24:11 -0800

    add env configs

--- node/letsencrypt.js
+++ node/letsencrypt.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const util  = require('util');
 const fs = require('fs');
 const { send } = require('./mail-core');
@@ -6,16 +7,18 @@ const domains = ['plex.hnxlabs.com', 'bt.hnxlabs.com'];
 
 const do_renew = async () => {
     try {
-    await exec('systemctl stop haproxy.service', { uid: 0});
-    await exec('letsencrypt renew --standalone', { uid: 0});
-    domains.forEach((domain) => {
-        const fullchain = fs.readFileSync(`/etc/letsencrypt/live/${domain}/fullchain.pem`);
-        const privKey = fs.readFileSync(`/etc/letsencrypt/live/${domain}/privkey.pem`);
-        fs.writeFileSync(`/etc/ssl/private/${domain}.pem`, `${fullchain}${privKey}`);
-    });
-    await exec('systemctl start haproxy.service', { uid: 0});
+        await exec('systemctl stop haproxy.service', { uid: 0});
+        await exec('letsencrypt renew --standalone', { uid: 0});
+        domains.forEach((domain) => {
+            const fullchain = fs.readFileSync(`/etc/letsencrypt/live/${domain}/fullchain.pem`);
+            const privKey = fs.readFileSync(`/etc/letsencrypt/live/${domain}/privkey.pem`);
+            fs.writeFileSync(`/etc/ssl/private/${domain}.pem`, `${fullchain}${privKey}`);
+        });
+        await exec('systemctl start haproxy.service', { uid: 0});
+        if (process.env.SEND_CERTS_MAIL === 'true'){
+            await send('Letsencrypt cron', 'Attempted to update certs');
+        }
     } catch(exception){
-        console.log('error while executing commands');
         console.log(exception);
     }
 }
