317f71905180837eabd07d208484b5f841251aa9
abrar-hnxlabs <abrar@hnxlabs.com>
2019-01-12 09:53:41 -0800

    buffer

--- node/plex.js
+++ node/plex.js
@@ -8,7 +8,11 @@ const exec = util.promisify(require('child_process').exec);
 const downloadUrl = 'https://plex.tv/downloads/latest/1?channel=16&build=linux-ubuntu-x86_64&distro=ubuntu&X-Plex-Token=4Jnu8SqixsEwiJFWK9D4';
 const tmpFileBin ='/tmp/plex.deb';
 const download_package = async () => {
-    const response = await axios.get(downloadUrl);
+    const response = await axios({
+        method: 'get',
+        responseType: 'arraybuffer',
+        url: downloadUrl
+    });
     fs.writeFileSync(tmpFileBin, response.data);
 }
 
@@ -20,11 +24,11 @@ const upgradeplex = async () => {
         await download_package();
         await exec(`dpkg -i ${tmpFileBin}`, { uid: 0 });
         if(process.env.SEND_PLEX_MAIL === 'true' ){
-            send('Plex upgrade', 'Attempted plex upgrade.');
+            send('Plex upgrade: Success', 'Attempted plex upgrade.');
         }
     } catch (exception) {
         if(process.env.SEND_PLEX_MAIL === 'true' ){
-            send('Plex upgrade', 'Attempted plex upgrade was failure <br />' + JSON.stringify(exception));
+            send('Plex upgrade: Failed', 'Attempted plex upgrade was failure <br />' + JSON.stringify(exception));
         }
         console.log(exception);
     } finally {
