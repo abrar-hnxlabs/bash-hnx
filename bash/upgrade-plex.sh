47a88dc4aabb3e9755637704cce06ca39a9e7ca5
abrar-hnxlabs <abrar@hnxlabs.com>
2019-01-12 09:56:37 -0800

    Delete upgrade-plex.sh

--- bash/upgrade-plex.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-date
-echo "plex: downloading"
-curl -sL -o ./plex.deb "https://plex.tv/downloads/latest/1?channel=16&build=linux-ubuntu-x86_64&distro=ubuntu&X-Plex-Token=4Jnu8SqixsEwiJFWK9D4"
-echo "plex: installing"
-sudo dpkg -i ./plex.deb
-echo "plex: install result $?"
-echo "plex: cleanup"
-rm ./plex.deb
