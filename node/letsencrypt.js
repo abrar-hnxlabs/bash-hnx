ad3ac8f2044a173fbf43a649c70a9df2f27e3432
abrar-hnxlabs <abrar@hnxlabs.com>
2019-01-12 07:55:36 -0800

    exec func

--- node/letsencrypt.js
+++ node/letsencrypt.js
@@ -3,5 +3,8 @@ const { send } = require('./mail-core');
 const exec = util.promisify(require('child_process').exec);
 
 const do_renew = async () => {
-    await exec('service haproxy stop', { uid: 0});
+    const { stdout } = await exec('systemctl stop haproxy.service', { uid: 0});
+    console.log(stdout);
 }
+
+do_renew();
