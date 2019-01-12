66904ec9b67952ef851ac24e61996f88430548ee
abrar-hnxlabs <abrar@hnxlabs.com>
2019-01-12 08:26:49 -0800

    delete shell script

--- bash/renew-certs.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-date
-sudo service haproxy stop
-sudo letsencrypt renew --standalone
-sudo cat /etc/letsencrypt/live/plex.hnxlabs.com/fullchain.pem /etc/letsencrypt/live/plex.hnxlabs.com/privkey.pem | sudo tee /etc/ssl/private/plex.hnxlabs.com.pem 
-sudo cat /etc/letsencrypt/live/bt.hnxlabs.com/fullchain.pem /etc/letsencrypt/live/bt.hnxlabs.com/privkey.pem | sudo tee /etc/ssl/private/bt.hnxlabs.com.pem
-sudo service haproxy start
