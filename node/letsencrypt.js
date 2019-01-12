cf58c4fd2f16103edbda723b85c97daa7c3751c2
abrar-hnxlabs <abrar@hnxlabs.com>
2019-01-12 08:09:02 -0800

    add file reads and writes

--- node/letsencrypt.js
+++ node/letsencrypt.js
@@ -1,10 +1,23 @@
 const util  = require('util');
+const fs = require('fs');
 const { send } = require('./mail-core');
 const exec = util.promisify(require('child_process').exec);
+const domains = ['plex.hnxlabs.com', 'bt.hnxlabs.com'];
 
 const do_renew = async () => {
-    const { stdout } = await exec('systemctl stop haproxy.service', { uid: 0});
+    try {
+    await exec('systemctl stop haproxy.service', { uid: 0});
+    await exec('letsencrypt renew --standalone', { uid: 0});
+    domains.forEach((domain) => {
+        const fullchain = fs.readFileSync(`/etc/letsencrypt/live/${domain}/fullchain.pem`);
+        const privKey = fs.readFileSync(`/etc/letsencrypt/live/${domain}/privkey.pem`);
+        fs.writeFileSync(`/etc/ssl/private/${domain}.pem `)
+    });
+    await exec('systemctl start haproxy.service', { uid: 0});
     console.log(stdout);
+    } catch(exception){
+        console.log('error while executing commands');
+    }
 }
 
 do_renew();
