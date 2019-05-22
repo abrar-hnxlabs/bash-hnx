db62a614f355a78bc94270379a76aff795b208b0
abrar-hnxlabs <abrar@hnxlabs.com>
2019-05-21 22:58:38 -0700

    Update handbrake.js

--- node/scripts/handbrake.js
+++ node/scripts/handbrake.js
@@ -7,7 +7,7 @@ const execEncode = async (inputfilePath) => {
     const baseDir = path.dirname(inputfilePath);
     const output = `${baseDir}/output-transcode-h265.mkv`;
     try {
-        const handbrake = spawn('/usr/bin/HandBrakeCLI', [`-f av_mkv -e x265_10bit -E eac3 -i "${inputfilePath}" -o "${output}"`], { stdio: 'inherit'});
+        const handbrake = spawn('/usr/bin/HandBrakeCLI', [`-f av_mkv -e x265_10bit -E eac3 -i '${inputfilePath}' -o '${output}'`], { stdio: 'inherit'});
         // await exec('/usr/bin/HandBrakeCLI', [`-f av_mkv -e x265_10bit -E eac3 -i "${inputfilePath}" -o "${output}"`]);
     } catch (e){
         console.log('Error while exec handbrake');
