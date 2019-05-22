b89dd0fd84de38c78df8c2c53a4832d5c950805e
abrar-hnxlabs <abrar@hnxlabs.com>
2019-05-22 04:44:56 -0700

    Update handbrake.js

--- node/scripts/handbrake.js
+++ node/scripts/handbrake.js
@@ -1,16 +1,16 @@
 const util = require('util');
 const path = require('path');
-const exec = util.promisify(require('child_process').execFile);
-const { spawn } = require('child_process');
+const exec = util.promisify(require('child_process').execFileSync);
+// const { spawn } = require('child_process');
 
 const execEncode = async (inputfilePath) => {
     const baseDir = path.dirname(inputfilePath);
     const output = `${baseDir}/output-transcode-h265.mkv`;
     try {
-        const handbrake = spawn('/usr/bin/HandBrakeCLI', [`-f av_mkv -e x265_10bit -E eac3 -i '${inputfilePath}' -o '${output}'`], { stdio: 'inherit'});
-        // await exec('/usr/bin/HandBrakeCLI', [`-f av_mkv -e x265_10bit -E eac3 -i "${inputfilePath}" -o "${output}"`]);
+        //const handbrake = spawn('/usr/bin/HandBrakeCLI', [`-f av_mkv -e x265_10bit -E eac3 -i '${inputfilePath}' -o '${output}'`], { stdio: 'inherit'});
+        await exec('/usr/bin/HandBrakeCLI', [`-f av_mkv -e x265_10bit -E eac3 -i "${inputfilePath}" -o "${output}"`], { stdio: 'inherit'} );
     } catch (e){
-        console.log('Error while exec handbrake');
+        console.log('Error while exec handbrake', e.code);
     }
 }
 
