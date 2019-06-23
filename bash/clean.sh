e6d55e287218de892a6e701fb563bee0a630c31a
abrar-hnxlabs <abrar@hnxlabs.com>
2019-06-23 00:20:27 -0700

    saving the clean script

--- bash/clean.sh
+++ bash/clean.sh
@@ -2,7 +2,7 @@
 
 packages=`apt list --installed | awk -F '/' '{ print $1 }'`
 
-if [ $1 == "-l" ]
+if [ "$1" == "-l" ]
 then
 	for pack in $packages
 	do 
@@ -13,8 +13,9 @@ fi
 
 for pack in $packages
 do
-	printf "Uninstall $pack ? y/n/q: \n"
+	printf "Uninstall $pack ? y/n/q:"
 	read -n 1 inputval
+	printf "\n"
 	if [ $inputval == "Y" ] || [ $inputval == "y" ]
 	then
 		sudo apt-get purge --auto-remove -y $pack
