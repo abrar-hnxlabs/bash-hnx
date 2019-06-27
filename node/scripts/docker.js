8a5adfe9d828f858e1547b7155f817d5664f4712
abrar-hnxlabs <abrar@hnxlabs.com>
2019-06-26 18:22:10 -0700

    adding code for docker restarts

---
+++ node/scripts/docker.js
@@ -0,0 +1,17 @@
+const util = require('util');
+const path = require('path');
+const exec = util.promisify(require('child_process').exec);
+
+const dockerRestart = async () => {
+    const dockerfile = path.resolve(__dirname, '..', '..', 'docker', 'docker-compose.yml');
+    log.info(`docker: restarting for -> ${dockerfile}`);
+    try {
+    await exec(`docker-compose -f dockerfile down`);
+    await exec(`docker-compose -f dockerfile up -d`);
+    } catch (e) {
+        log.error('Error while restarting docker.')
+    }
+    log.info(`docker: done restart`)
+}
+
+module.exports = { dockerRestart }
