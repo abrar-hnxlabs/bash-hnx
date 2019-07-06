5f529e78bc7dc203f3599c452beef3d98bdfae26
abrar-hnxlabs <abrar@hnxlabs.com>
2019-07-05 20:32:00 -0700

    Update init-system.sh

--- bash/init-system.sh
+++ bash/init-system.sh
@@ -10,17 +10,8 @@ read answer
 if [ "$answer" == "Y" ] || [ "$answer" == "y" ]; then
   sudo apt-get update
   sudo apt-get install -y \
-        		software-properties-common \
-		        samba \
+  	        samba \
 			      whois
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-  sudo apt-key fingerprint 0EBFCD88
-  sudo add-apt-repository \
-    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-    $(lsb_release -cs) \
-    stable"
-  sudo apt-get update
-  sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose
   curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
   sudo apt-get install -y nodejs
   
